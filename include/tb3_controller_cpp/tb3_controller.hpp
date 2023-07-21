258edb05b24cdf8aa9ecf376f727d9a389a830e0
kj_shimz_omen <kj_shimz_omen@shimz>
2023-07-21 09:17:56 +0900

    ADD:timer_callback

--- include/tb3_controller_cpp/tb3_controller.hpp
+++ include/tb3_controller_cpp/tb3_controller.hpp
@@ -15,12 +15,20 @@ public:
 private:
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
   rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr xd_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
   void xd_callback(std_msgs::msg::Float32::SharedPtr msg);
+  void vel_callback(geometry_msgs::msg::Twist::SharedPtr msg);
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
   void scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg);
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   void odom_callback(nav_msgs::msg::Odometry::SharedPtr msg);
+  void timer_callback();
   float Kp_;
+  float Kd_;
   float xd_;
+  float vd_;
+  float x_;
+  float v_;
   geometry_msgs::msg::Pose current_pose_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };

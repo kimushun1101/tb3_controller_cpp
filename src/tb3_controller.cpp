28194ee6b2ecfacc5977ca1d8b6c9bd0422feaaa
Shunsuke Kimura <kimushun1101@gmail.com>
2023-07-25 18:31:11 +0900

    separate files into class and node

--- src/tb3_controller.cpp
+++ src/tb3_controller.cpp
@@ -1,82 +1,71 @@
+//  Copyright 2023 Shunsuke Kimura
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 #include "tb3_controller_cpp/tb3_controller.hpp"
 
-using std::placeholders::_1;
-using namespace std::chrono_literals;
-
-Tb3Controller::Tb3Controller()
-: Node("tb3_controller")
+Tb3Controller::Tb3Controller() : Node("tb3_controller")
 {
-  this->declare_parameter<std::float_t>("Kp", 0.1);
+  this->declare_parameter<std::float_t>("init_xd", 3.0);
+  this->get_parameter("init_xd", xd_);
+  this->declare_parameter<std::float_t>("Kp", 1.0);
   this->get_parameter("Kp", Kp_);
-  this->declare_parameter<std::float_t>("Kd", 0.01);
+  this->declare_parameter<std::float_t>("Kd", 1.0);
   this->get_parameter("Kd", Kd_);
-  xd_ = 3.0; //　なんで型指定してないのに動いているのだろう？->ヘッダで定義しているから
-  vd_ = 0.0;
+  this->declare_parameter<std::float_t>("T", 0.001);
+  this->get_parameter("T", T_);
+  std::chrono::milliseconds sampling_period{(int)(T_*1000.0)};
+  x_ = xd_; // /scan トピックが取得されるまでは，目標値と一致させておくことで制御入力を0とする
+  e_pre_ = 0.0;
+
+  using std::placeholders::_1;
   xd_sub_ = this->create_subscription<std_msgs::msg::Float32>(
     "/xd", rclcpp::QoS(10), std::bind(&Tb3Controller::xd_callback, this, _1));
   scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
     "/scan", rclcpp::SensorDataQoS(), std::bind(&Tb3Controller::scan_callback, this, _1));
-  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-    "/odom", rclcpp::QoS(10), std::bind(&Tb3Controller::odom_callback, this, _1));
-  cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10); // これはpublishされてる？->scan_callbackでされてる
-  cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-    "/cmd_vel", rclcpp::QoS(10),  std::bind(&Tb3Controller::vel_callback, this, _1));
+  cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
   timer_ = this->create_wall_timer(
-      1ms, std::bind(&Tb3Controller::timer_callback, this));
-  // プログラム内部のパラメータを確認する方法はpublishするぐらいしかない？あまりにも手間がかかる…->RCLCPP_INFOがある
-  // QoS(10)とかSensorDataQoS()はどのような意味があるの？
-  // 同じcall_back関数に複数のtopicのsubscriptionを設定したい
-  // timer_callbackはどうやって使うのか？
+    sampling_period, std::bind(&Tb3Controller::timer_callback, this));
 
   RCLCPP_INFO(this->get_logger(), "tb3_controller node has been initialised");
+  RCLCPP_INFO_STREAM(this->get_logger(), "initial xd : " << xd_);
+  RCLCPP_INFO_STREAM(this->get_logger(), "Kp : " << Kp_);
+  RCLCPP_INFO_STREAM(this->get_logger(), "Kd : " << Kd_);
+  RCLCPP_INFO_STREAM(this->get_logger(), "T : " << T_);
 }
 
 void Tb3Controller::xd_callback(std_msgs::msg::Float32::SharedPtr msg)
 {
-  xd_ = msg->data;
+  if(xd_ != msg->data)
+  {
+    xd_ = msg->data;
+    RCLCPP_INFO_STREAM(this->get_logger(), "x_d has been updated : " << xd_);
+  }
 }
 
-void Tb3Controller::timer_callback() //ここにコントローラを設計して出力するようにすれば良さそう
+void Tb3Controller::scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
-  // RCLCPP_INFO(this->get_logger(), "Hello!!");
+  // 0 ~ 2\pi まで360点取得している
+  // RCLCPP_INFO_STREAM(this->get_logger(), "point amount : " << msg->ranges.size());
+  // x_ = msg->ranges[0];  // 正面の距離を計測
+  x_ = msg->ranges[180];  // 背面の距離を計測
+}
+
+void Tb3Controller::timer_callback()
+{
   auto message = geometry_msgs::msg::Twist();
-  message.linear.x = - Kp_ * (x_ - xd_) - Kd_ * (v_ - vd_);
-  RCLCPP_INFO(this->get_logger(), "I heard: '%f'", message.linear.x);
+  auto e = x_ - xd_;
+  message.linear.x = - Kp_ * e - Kd_ * (e - e_pre_)/T_;
+  e_pre_ = e;
   cmd_vel_pub_->publish(message);
-}
-//, geometry_msgs::msg::Twist::SharedPtr msg1
-void Tb3Controller::scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg) // 速度Dと積分Iをどうやって実装すればよいのか？->callback関数の中でコントローラを組むのは不可能？
-{
-  x_ = msg->ranges[0];  // 正面の距離を計測
-  // 背面の距離を計測したい場合
-  // auto point_count = msg->ranges.size();
-  // x_ = msg->ranges[point_count / 2];
-}
-
-void Tb3Controller::odom_callback(nav_msgs::msg::Odometry::SharedPtr msg)
-{
-  current_pose_ = msg->pose.pose;
-}
-
-void Tb3Controller::vel_callback(geometry_msgs::msg::Twist::SharedPtr msg)
-{
-  v_ = msg->linear.x;
-  // RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->linear.x);
-}
-
-
-int main(int argc, char * argv[])
-{
-  signal(
-    SIGINT, [](int) {
-      auto node = rclcpp::Node::make_shared("stop");
-      auto pub = node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
-      pub->publish(geometry_msgs::msg::Twist());
-      rclcpp::shutdown();
-    });
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<Tb3Controller>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
 }
